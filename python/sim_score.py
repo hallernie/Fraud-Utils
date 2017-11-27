97e653e82e94dfcdd2690b6795f4d9ea98d0c5f1
hallernie <hallernie@hotmail.com>
2017-11-27 17:06:57 -0500

    Add files via upload

--- python/sim_score.py
+++ python/sim_score.py
@@ -5,7 +5,7 @@ import csv
 #
 def simulate(str_ruleweights_filename, str_rules_filename):
     # Store rulenames/weights in a hash
-    # The rules file is in the following format:
+    # The ruleweights file is in the following format:
     #           RuleName,RuleWeight
     #           ------------- HEADER -------------,0
     #           INVALID_account_address_zip,0
@@ -79,9 +79,91 @@ def simulate(str_ruleweights_filename, str_rules_filename):
 
 
 #
+# This is the simulator functionality. The only difference is that
+# we assume that there are rules in the rules file that are not in the
+# lookup table.
+#
+def simulate2(str_ruleweights_filename, str_rules_filename):
+    # Store rulenames/weights in a hash
+    # The ruleweights file is in the following format:
+    #           RuleName,RuleWeight
+    #           ------------- HEADER -------------,0
+    #           INVALID_account_address_zip,0
+    #           UnusualProxyAttributes,-10
+    #           SmartIDValid,0
+    #           ExactIDInvalid,0
+    #           MobileDevice,0
+    #           ------------- LOCAL BLACKLISTS -------------,0
+    #           DeviceOnLocalBlacklist,-100
+    #           FuzzyDeviceOnLocalBlacklist,-100
+    #           TrueIPOnLocalBlacklist,-100
+    #           TrueIPGeoOnLocalBlacklist,-100
+    #
+    f_ruleweights = open(str_ruleweights_filename)
+    next(f_ruleweights) # Skip the header row
+
+    h_results_weights = {} # Lookup table for rules/weights
+    for val in f_ruleweights:
+        l_val = next(csv.reader([val.strip()]))
+        str_rulename = l_val[0]
+        str_ruleweight = l_val[1]
+        h_results_weights[str_rulename] = int(str_ruleweight)
+
+    # Read the rules file and calculate the policy score.
+    # The rules file is in the following format:
+    #       Request ID,Rules,ruleweight
+    #       62fb6a5682e04c97a466a0e3c050bc63,DeviceLocalAgeLessThanOneHour,0
+    #       62fb6a5682e04c97a466a0e3c050bc63,FuzzyDeviceLocalAgeLessThanOneHour,0
+    #       71145c2a008945a58d3083d8a94959ad,DeviceLocalAgeLessThanOneHour,0
+    #       71145c2a008945a58d3083d8a94959ad,DeviceGlobalAgeLessThanOneHour,0
+    #       71145c2a008945a58d3083d8a94959ad,FuzzyDeviceLocalAgeLessThanOneHour,0
+    #       8a03e707b5104628a206f2c2c60f955a,FlashDisabled,-5
+    # Store results in a hash
+    #
+    f_rules = open(str_rules_filename)
+    next(f_rules) # Skip the header row
+
+    h_results = {}
+    for val in f_rules:
+        l_val = next(csv.reader([val.strip()]))
+        str_requestid = l_val[0]
+        str_rulename = l_val[1]
+
+        if not (str_rulename in h_results_weights):
+            pass # Assumption is that this rule is not part of simulation
+        elif str_requestid in h_results:
+            if str_rulename == 'no_rules':
+                pass
+            else:
+                score = h_results[str_requestid] + h_results_weights[str_rulename]
+                h_results[str_requestid] = score
+        else:
+            if str_rulename == 'no_rules':
+                h_results[str_requestid] = 0
+            else:
+                weight = h_results_weights[str_rulename]
+                h_results[str_requestid] = weight
+
+    # Iterate throught the results and set all values -101
+    # and less to "-100". Also set values 101 or greater to "100"
+    for key,value in h_results.items():
+        if h_results[key] < -100:
+            h_results[key] = -100
+        if h_results[key] > 100:
+            h_results[key] = 100
+
+
+    # Output the values from h_results. These are the calculated
+    # policy scores
+    print('Request ID,Calc Policy Score')
+    for key,value in h_results.items():
+        print("{0},{1}".format(key,value))
+
+
+#
 # Checks that the rules from the events file are contained in the lookup
 # file containing the rule/weights. You should add any rules output
-# from the function the rule/weights lookup file.
+# from the function to the rule/weights lookup file.
 #
 def check_rules(str_ruleweights_filename, str_rules_filename):
     # Read rule lookup into a hash
@@ -112,5 +194,5 @@ def check_rules(str_ruleweights_filename, str_rules_filename):
         print(key)
 
 #***MAIN***
-#simulate('ruleweights.csv', 'irc_Rules.csv')
-check_rules('ruleweights.csv', 'irc_Rules.csv')
+simulate2('ruleweights.csv', 'total_RulesJun.csv')
+#check_rules('ruleweights.csv', 'total_RulesJun.csv')
