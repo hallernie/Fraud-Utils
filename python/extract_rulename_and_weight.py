4af7ef2d8d8abfdc73a5b445eb5ce18ef417b96f
hallernie <hallernie@hotmail.com>
2018-03-19 19:19:36 -0400

    Add files via upload

--- python/extract_rulename_and_weight.py
+++ python/extract_rulename_and_weight.py
@@ -1,5 +1,6 @@
 import sys
 import json
+import re
 
 #
 #   Counts the number of rules in a ThreatMetrix json policy file.
@@ -17,11 +18,12 @@ def countRules(l_rules):
 # Takes the policy json file from ThreatMetrix and extracts:
 #       RuleName,RuleWeight
 def get_rulename_and_weight(l_rules):
+    re_if_statement = re.compile(r"IF#")
     for val in l_rules:
-        if(val["ruleId"] not in [120,121,122]): # ruleId 121 is "Terminate" rule.
+        if re_if_statement.search(val['id']):
+            get_rulename_and_weight(val['rules'])
+        else: 
             print("{0},{1}".format(val['displayName'], val['riskWeight']))
-        elif("rules" in val.keys()): # if not in, then this IF statement has no internal rules
-            get_rulename_and_weight(val["rules"])
 
 
 ### MAIN ####
@@ -38,7 +40,7 @@ for val in open(str_json_policy_filename):
 policy = json.loads(str_val)
 
 # A "rule" consists of a list of maps
-l_rules = policy["policyVersion"]["rulesSet"]["rules"]
+l_rules = policy["policyVersion"]["rules"]
 
 # Print header row
 print("RuleName,RuleWeight")
