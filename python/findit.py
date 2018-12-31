6042dfa92f3f1bf56d3a04ed0e4deda56cf40e09
hallernie <hallernie@hotmail.com>
2018-12-31 15:05:35 -0500

    Delete findit.py

--- python/findit.py
+++
@@ -1,8 +0,0 @@
-#
-# Takes a sequence plus a string value. Checks each
-# value in the sequence to see if it contains the giving string.
-#
-def findit(seq,s_tofind):
-    for val in seq:
-        if s_tofind in str(val).lower():
-            print(val)
