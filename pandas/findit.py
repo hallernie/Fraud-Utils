436068a143ea9151aa93fe67a955d12753c72d76
hallernie <hallernie@hotmail.com>
2019-01-02 14:54:56 -0500

    Add files via upload

--- pandas/findit.py
+++ pandas/findit.py
@@ -3,6 +3,23 @@
 # value in the sequence to see if it contains the giving string.
 #
 def findit(seq,s_tofind):
+    """def findit(seq,s_tofind):
     for val in seq:
         if s_tofind in str(val).lower():
             print(val)
+    """
+    for val in seq:
+        if s_tofind in str(val).lower():
+            print(val)
+
+
+#
+# Takes a sequence and prints its values one per line
+#
+def printit(seq):
+    """def printit(seq):
+    for x,val in enumerate(seq):
+        print("    {0}:   {1}".format(x,str(val)))
+    """
+    for x,val in enumerate(seq):
+        print("    {0}:   {1}".format(x,str(val)))
