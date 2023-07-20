faee4e94aa3eda34ee8ad28c9eb66839b4b404d0
hallernie <hallernie@hotmail.com>
2023-07-19 17:11:39 -0700

    Add files via upload

--- real_estate/tt.py
+++ real_estate/tt.py
@@ -1,4 +1,5 @@
 import re
+import numpy as np
 
 # Open the question bank for reading
 #f_input = open('unit_2.txt')
@@ -19,7 +20,7 @@ choice_letter_re = re.compile(r"^[A-D][)]")
 blank_line_re = re.compile(r"^$")
 
 
-##### function to store questions and answers
+##### method to store questions and answers
 #
 # Input is a file containing questions and answers:
 #
@@ -74,14 +75,18 @@ def get_questions_and_answers(str_filename):
     total_list = []
     tmp_list =[]
 
-    # find the first question
     for val in f_input:
+        # find the first question
         if question_re.match(val):
             break;
+        # store the unit number and description
+        if unit_re.match(val):
+            str_unit = val.strip()
 
     # store the first question
     tmp_list.append(val.strip())
 
+    # Continue processing the file
     for val in f_input:
         #skip blank lines
         if blank_line_re.match(val):
@@ -89,11 +94,14 @@ def get_questions_and_answers(str_filename):
         elif question_re.match(val):
             # add the correct answer to the end of the list
             tmp_list.append(str_correct_answer)
-            # add the question, choices, and answer to the total
+            # add the unit # and description to the end of the list
+            tmp_list.append(str_unit)
+            # add the question, choices, correct answer, and unit number/desc to the total
             total_list.append(tmp_list)
 
-            # start a new set
+            # start a new question
             tmp_list = []
+            # append the new question to the beginning of the list
             tmp_list.append(val.strip())
         elif choice_letter_re.match(val):
             str_choice = ""
@@ -102,19 +110,32 @@ def get_questions_and_answers(str_filename):
                 str_correct_answer = val[0]
             else:
                 str_choice += val.strip()
-        else: # must be a choice
+        else: # must be the choice text
             str_choice += f" {val.strip()}"
             tmp_list.append(str_choice)
 
     # Store the last question/answer set
     tmp_list.append(str_correct_answer)
+    tmp_list.append(str_unit)
     total_list.append(tmp_list)
 
     return total_list
 ##### end: get_questions_and_answers(str_filename)
 
 
+##### method to present one question, prompt for answer, and display if answer was correct
+#
+# Input is a list in the form:
+#
+# 	[question, choice1, choice2, choice3, choice4, correct_answer]
+#
+"""
+['3. The person who prepares an abstract of title for a parcel of real estate', 'A) insures the condition of the title.', 'B) inspects the property.', 'C) issues title insurance.', 'D) searches the public records and then summarizes the events and proceedings that affect title.', 'D']
+"""
+#
 def present_question(l_question):
+    print(f"### {l_question[6]} ###")
+    print()
     print(l_question[0])
     print()
     print(l_question[1])
@@ -134,13 +155,39 @@ def present_question(l_question):
 ##### end: present_question(l_question)
 
 
+##### method to randomly deliver test questions
+#
+# units: "0" for all units, or an integer corresponding to the unit #
+# int_num_questions: the number of questions to ask
+# int_random_seed: an integer random seed, used to shuffle the questions
+#
+def process_questions(l_question_bank, units, int_num_questions, int_random_seed):
+    if units == '0':
+        np.random.seed(int_random_seed)
+        idx = np.arange(len(l_question_bank))
+        np.random.shuffle(idx)
+
+        if int_num_questions > len(l_question_bank):
+            int_num_question = len(l_question_bank)
+
+        for i in range(int_num_questions):
+            present_question(l_question_bank[idx[i]])
+##### end: process_questions()
+
+
+## MAIN ##
 filename = input("Enter the test file name: ")
+num_questions = input("Enter length of test: ")
+unit_number = input("Enter unit number, or 0 for all units: ")
+random_seed = input("Enter a random integer: ")
+
 questions = get_questions_and_answers(filename)
-for x in questions:
-    present_question(x)
+process_questions(questions, unit_number, int(num_questions), int(random_seed))
 
-
-# One pass through the file to build up test bank data structure
-#for x in f_input:
-    #if question_re.match(x):
-        #print(x.strip())
+## TO DO ##
+#
+# Keep track of correct and incorrect
+#----- give % correct
+#----- list incorrect answers (unit and question #)
+#----- summary by unit
+#--------- % correct for each unit
